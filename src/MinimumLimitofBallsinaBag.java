d3fbce9ca37c92f57da21dbb193cb11fd21c567a
sahilsanaik <sahilsantoshnaik3112004wed@gmail.com>
2024-12-08 15:40:59 +0530

    commit

---
+++ src/MinimumLimitofBallsinaBag.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MinimumLimitofBallsinaBag {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt(), result = right;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canDivide(nums, maxOperations, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    private boolean canDivide(int[] nums, int maxOperations, int penalty) {
+        int operations = 0;
+        for (int balls : nums) {
+            if (balls > penalty) operations += (balls - 1) / penalty;
+            if (operations > maxOperations) return false;
+        }
+        return true;
+    }
+}
